24c7e134f27c880b2aed815294653fb5a177e639
Rodrigo Emanuel <drigosantos@gmail.com>
2022-11-06 12:12:04 -0300

    Finish Signin Interface Mobile

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -7,6 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&family=Roboto:wght@100;300;400;700&display=swap" rel="stylesheet" />
+        <title>Bolão Copa 2022</title>
       </Head>
       <body className='bg-gray-900 bg-app bg-no-repeat bg-cover'>
         <Main />
